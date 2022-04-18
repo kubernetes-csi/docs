6bc7eaa8e61ae168a7dfa63c679bc1fa757c983e
Raunak Pradip Shah <raunakshah13@gmail.com>
2022-04-18 14:52:20 +0530

    Documentation to prevent volume mode conversion

--- hack/gen-api.sh
+++ hack/gen-api.sh
@@ -28,11 +28,11 @@ git clone git@github.com:ahmetb/gen-crd-api-reference-docs.git "$gen_tool_dir"
 git clone git@github.com:kubernetes-csi/external-snapshotter.git "$csi_dir"
 
 cd "$gen_tool_dir"
-git co "$GEN_TOOL_REF"
+git checkout "$GEN_TOOL_REF"
 go build "$gen_tool_dir"
 
 cd "$csi_dir"
-git co "$CSI_REF"
+git checkout "$CSI_REF"
 $gen_tool_dir/gen-crd-api-reference-docs \
   -config="$curr_dir/$config_path" \
   -api-dir="./client/apis/volumesnapshot/v1" \
